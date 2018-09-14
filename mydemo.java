b902947bc4ca389f154f9f84775a77d4111b018b
Chammoisten <43243648+Chammoisten@users.noreply.github.com>
2018-09-14 20:27:29 +0800

    Add files via upload

---
+++ mydemo.java
@@ -0,0 +1,20 @@
+
+public class mydemo {
+   public static void main(String[] args)
+   {
+	   int num=99;
+	   String word="bottles";
+		while(num>0)	
+		{if(num==0)
+		{word="bottle";}
+		System.out.println(num+" "+word+"of beer on the wall");
+		System.out.println(num+" "+word+"of beer");
+		System.out.println("Take one down.");
+		System.out.println("Pass it around.");
+		num=num-1;
+		if(num>0)
+		{System.out.println(num+" "+word+"of beer on the wall");}
+		else {System.out.println("no more bottles of beer on the wall");}
+}
+   }
+      }
